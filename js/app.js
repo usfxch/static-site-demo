5c0c4704c0a52537b851f8fe7c07abd76c5e7cc6
Marcelo Quispe Ortega <marceloquispeortega@gmail.com>
2026-05-17 21:32:10 -0400

    feat: inicializa sitio web estatico

---
+++ js/app.js
@@ -0,0 +1,4 @@
+document.getElementById('btn').addEventListener('click', () => {
+    document.getElementById('message').textContent =
+        '¡Desplegado automáticamente con GitHub Actions!';
+});
